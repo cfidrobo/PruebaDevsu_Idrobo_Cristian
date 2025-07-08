503953e78887d1d8eff4f4456b80fb1685ac0594
cfidrobo <cfidrobo@espe.edu.ec>
2025-07-07 20:07:00 -0500

    fix: ajustes para evitar logs en tests y mejorar entorno de ejecución en CI

--- shared/database/database.js
+++ shared/database/database.js
@@ -3,9 +3,12 @@ import { Sequelize } from 'sequelize'
 
 dotenv.config()
 
+const isTest = process.env.NODE_ENV === 'test'
+
 const sequelize = new Sequelize('test-db', process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
   dialect: 'sqlite',
-  host: process.env.DATABASE_NAME
+  host: process.env.DATABASE_NAME,
+  logging: isTest ? false : console.log
 })
 
 export default sequelize
